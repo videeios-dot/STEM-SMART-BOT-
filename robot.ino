8a2c3f9c0a51a3b845da8b8927ef526f4136477c
videeios-dot <videeios@gmail.com>
2026-09-17 13:40:22 +0200

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,15 +1,35 @@
 
-int frontLeftMotor = .... ;
-int frontRightMotor = .... ;
-int backLeftMotor = .... ;
-int backRightMotor = .... ; 
-int trig = .... ; 
-int traceSensor = .... ; 
-int ledOne = .... ;
-int ledTwo = .... ; 
-int echo =....; 
+// =========================
 
+// PIN / MOTOR NAMES
 
+// =========================
+
+// Motor channels on the Acebott shield
+
+int frontLeftMotor  = 1;   // M1
+
+int frontRightMotor = 2;   // M2
+
+int backLeftMotor   = 3;   // M3
+
+int backRightMotor  = 4;   // M4
+
+// Ultrasonic sensor
+
+int trig = 13;
+
+int echo = 14;
+
+// Trace sensor
+
+int traceSensor = 35;
+
+// LEDs
+
+int ledOne = 2;
+
+int ledTwo = 12;
 
  void moveForward(){
 moveMotor(frontLeftMotor, FORWARD);
@@ -17,9 +37,39 @@ moveMotor(frontRightMotor, FORWARD);
 moveMotor(backLeftMotor, FORWARD);
 moveMotor(backRightMotor, FORWARD);
 }
-void steerLeft(90){
+void turnLeft(90){
+moveMotor(frontLeftMotor, BACKWARD);
+moveMotor(frontRightMotor, FORWARD); 
+moveMotor(backLeftMotor, BACKWARD);
+moveMotor(backRightMotor, FORWARD);
 }
-void stopMotos(){
+
+void turnRight(90){
+moveMotor(frontRightMotor, BACKWARD);
+moveMotor(frontLeftMotor, FORWARD); 
+moveMotor(backRightMotor, BACKWARD);
+moveMotor(backLeftMotor, FORWARD);
+}
+long readDistance(){
+digitalWrite(trig, LOW);
+delayMicroseconds(2);
+
+digitalWrite(trig, HIGH);
+delayMicroseconds(10);
+
+digitalWrite(trig, LOW);
+ 
+duration = pulseIn(echo, HIGH);
+distance= duration*0.0343 / 2
+return distance;
+}
+
+
+void stopMotors(){
+moveMotor(frontLeftMotor, STOP);
+moveMotor(frontRightMotor, STOP); 
+moveMotor(backLeftMotor, STOP);
+moveMotor(backRightMotor, STOP);
 }
 
 
@@ -29,23 +79,24 @@ pinMode(frontLeftMotor, OUTPUT);
 pinMode(frontRightMotor, OUTPUT);
 pinMode(backLeftMotor, OUTPUT);
 pinmode(backRightMotor, OUTPUT);
+
 pinMode(trig, OUTPUT);
-//int traceSensor = .... ; 
+pinMode(echo, INPUT);
+
 pinmode(ledOne, OUTPUT);
 pinmode(ledTwo, OUTPUT);
+
+stopMotors();
 
 }
 
 void loop(){
-
-digitalWrite(trig, HIGH);
-//distance = read length 
+int distance = readDistance; 
 
 if(distance > 10) {
 moveForward();
- } else if (distance< 10){
+ } else(distance< 10){
 stopMotors();
 steerLeft(90);
-}else {
 };
 }
