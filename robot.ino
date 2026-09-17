f4d28245504afa0db482619d498c6033dc3a18e9
videeios-dot <videeios@gmail.com>
2026-09-17 13:11:25 +0200

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,8 +1,51 @@
+
+int frontLeftMotor = .... ;
+int frontRightMotor = .... ;
+int backLeftMotor = .... ;
+int backRightMotor = .... ; 
+int trig = .... ; 
+int traceSensor = .... ; 
+int ledOne = .... ;
+int ledTwo = .... ; 
+int echo =....; 
+
+
+
  void moveForward(){
+moveMotor(frontLeftMotor, FORWARD);
+moveMotor(frontRightMotor, FORWARD); 
+moveMotor(backLeftMotor, FORWARD);
+moveMotor(backRightMotor, FORWARD);
+}
+void steerLeft(90){
+}
+void stopMotos(){
 }
 
+
 void setup() {
+
+pinMode(frontLeftMotor, OUTPUT);
+pinMode(frontRightMotor, OUTPUT);
+pinMode(backLeftMotor, OUTPUT);
+pinmode(backRightMotor, OUTPUT);
+pinMode(trig, OUTPUT);
+//int traceSensor = .... ; 
+pinmode(ledOne, OUTPUT);
+pinmode(ledTwo, OUTPUT);
+
 }
 
 void loop(){
+
+digitalWrite(trig, HIGH);
+//distance = read length 
+
+if(distance > 10) {
+moveForward();
+ } else if (distance< 10){
+stopMotors();
+steerLeft(90);
+}else {
+};
 }
